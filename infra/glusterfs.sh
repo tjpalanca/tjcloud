dad6b23d65b009cdfbc3a085fb990454185a43d5
TJ Palanca <mail@tjpalanca.com>
2021-06-19 16:50:29 +0000

    Update to new namespaces and server names

--- infra/glusterfs.sh
+++ infra/glusterfs.sh
@@ -1,6 +1,6 @@
 # Set up glusterfs on the cluster
 
-cx ssh -s 'tjpalanca.com' master01
+cx ssh -s 'tjpalanca' master
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common
@@ -11,22 +11,22 @@ sudo systemctl status glusterd.service
 
 sudo gluster peer status
 
-sudo gluster volume create tjpalanca master01.tjpalanca-com.c66.me:/mnt/gluster force
-sudo gluster volume start tjpalanca
+sudo gluster volume create storage master.tjpalanca.c66.me:/mnt/storage01 force
+sudo gluster volume start storage
 sudo gluster volume status
 
 sudo apt install glusterfs-client
 sudo mkdir /mnt/storage
 
-sudo mount -t glusterfs master01.tjpalanca-com.c66.me:tjpalanca /mnt/storage
+sudo mount -t glusterfs localhost:storage /mnt/storage
 
 sudo su
-echo 'master01.tjpalanca-com.c66.me:/tjpalanca /mnt/storage glusterfs defaults,_netdev 00' >> /etc/fstab
+echo 'localhost:/storage /mnt/storage glusterfs defaults,_netdev 00' >> /etc/fstab
 exit
 
 exit
 
-cx ssh -s 'tjpalanca.com' kube01
+cx ssh -s 'tjpalanca' dev01
 
 sudo apt-get update
 sudo apt-get install -y software-properties-common
@@ -36,10 +36,10 @@ sudo apt-get update
 sudo apt install -y glusterfs-client
 sudo mkdir /mnt/storage
 
-sudo mount -t glusterfs master01.tjpalanca-com.c66.me:tjpalanca /mnt/storage
+sudo mount -t glusterfs master.blechnum.apn:storage /mnt/storage
 
 sudo su
-echo 'master01.tjpalanca-com.c66.me:/tjpalanca /mnt/storage glusterfs defaults,_netdev 00' >> /etc/fstab
+echo 'master.blechnum.apn:storage:/storage /mnt/storage glusterfs defaults,_netdev 00' >> /etc/fstab
 exit
 
 exit
