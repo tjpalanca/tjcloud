24a0b8030e2d96b1b91ba034449db3375aa4f18b
TJ Palanca <mail@tjpalanca.com>
2021-10-06 17:23:12 +0000

    Add postgres optimizations

---
+++ system/glusterfs-postgres.sh
@@ -0,0 +1,16 @@
+# https://www.bookstack.cn/read/okd-v3.11/6e7ee7cd455344a2.md
+
+cx ssh -s 'tjcloud' master
+
+sudo su
+gluster volume list
+gluster volume set storage performance.stat-prefetch off
+gluster volume set storage performance.read-ahead off
+gluster volume set storage performance.write-behind off
+gluster volume set storage performance.readdir-ahead off
+gluster volume set storage performance.io-cache off
+gluster volume set storage performance.quick-read off
+gluster volume set storage performance.open-behind off
+
+exit
+exit
