fe62cbbab99f8eda56a6dc58845aae1966a207b6
TJ Palanca <mail@tjpalanca.com>
2021-12-10 12:05:21 +0000

    Add tailscale to gitpod instance

---
+++ gitpod/install/tailscale.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash 
+
+# Install tailscale
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add - \
+     && curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list \
+     && apt-get update \
+     && apt-get install -y tailscale
+    
