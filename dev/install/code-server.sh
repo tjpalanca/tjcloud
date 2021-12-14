155d095df859f04189af18b16e3a95e2ef859fec
TJ Palanca <mail@tjpalanca.com>
2021-12-14 05:41:36 +0000

    Up to 4.0.0 trial

--- dev/install/code-server.sh
+++ dev/install/code-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-CODE_SERVER_VERSION="3.12.0"
+CODE_SERVER_VERSION="4.0.0"
 
 wget "https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb" \
 	-O code-server.deb 
