009c1132c927f2e423ec284c02751b492bea50b3
TJ Palanca <mail@tjpalanca.com>
2021-12-18 15:43:33 +0000

    Downgrade back and minor fixes

--- dev/install/code-server.sh
+++ dev/install/code-server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-CODE_SERVER_VERSION="3.12.0"
+VERSION="3.12.0"
 
-# wget "https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb" \
-# 	-O code-server.deb 
-# This is a temp location while waiting for official 4.0.0 release
-wget https://tjpalanca.sgp1.digitaloceanspaces.com/bin/code-server_4.0.0_amd64.deb -O code-server.deb
-dpkg -i code-server.deb 
+wget "https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb" \
+	-O code-server.deb
+# wget "https://tjpalanca.sgp1.digitaloceanspaces.com/bin/code-server_${VERSION}_amd64.deb" \
+#     -O code-server.deb
+dpkg -i code-server.deb
 rm code-server.deb
