c346dfeb7b5c8e187ba8b68220f87a040ae3cd44
TJ Palanca <mail@tjpalanca.com>
2021-12-18 07:48:42 +0000

    Change to main branch of code-server 4.0.0

--- dev/install/code-server.sh
+++ dev/install/code-server.sh
@@ -2,7 +2,9 @@
 
 CODE_SERVER_VERSION="3.12.0"
 
-wget "https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb" \
-	-O code-server.deb 
+# wget "https://github.com/cdr/code-server/releases/download/v${CODE_SERVER_VERSION}/code-server_${CODE_SERVER_VERSION}_amd64.deb" \
+# 	-O code-server.deb 
+# This is a temp location while waiting for official 4.0.0 release
+wget https://tjpalanca.sgp1.digitaloceanspaces.com/bin/code-server_4.0.0_amd64.deb -O code-server.deb
 dpkg -i code-server.deb 
 rm code-server.deb
---
+++ dev/install/s3cmd.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get install -y s3cmd
