e0c79efd8413d0c2687b8cf98e4db21babeb4ff9
TJ Palanca <mail@tjpalanca.com>
2021-12-19 00:47:53 +0000

    Fixes to perms and dockerifles

--- dev/install/gitpod.sh
+++ dev/install/gitpod.sh
@@ -2,6 +2,8 @@
 
 # Create gitpod user
 useradd -l -u 33333 -G sudo -md /home/gitpod -s /usr/bin/zsh -p gitpod gitpod 
+adduser gitpod sudo 
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 # Create .zshrc so zsh doesn't complain 
 touch ~/.zshrc
