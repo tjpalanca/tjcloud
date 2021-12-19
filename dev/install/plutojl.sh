8108df62cb6b8590814780bbc8114174063a1f80
TJ Palanca <mail@tjpalanca.com>
2021-12-19 09:03:44 +0000

    Cleanup and harmonization

--- dev/install/plutojl.sh
+++ dev/install/plutojl.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 
-JULIA_DEPOT_PATH="/opt/julia/local/share/julia"
-mkdir -p $JULIA_DEPOT_PATH
-JULIA_DEPOT_PATH=$JULIA_DEPOT_PATH julia -e 'import Pkg; Pkg.add("Pluto")'
+julia -e 'import Pkg; Pkg.add("Pluto")'
