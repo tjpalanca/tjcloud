02f858cbe93cfbcc1a6828ce4d9545acab74824d
TJ Palanca <mail@tjpalanca.com>
2021-12-19 06:48:09 +0000

    Settling on a new default
    * Moved to openvscode-server from code-server due to slow update issues on code-server end, and overall gitpod's offerings are great.
    * Make Pluto installation not conflict with gitpod prebuild step
    * Default openvscode-server connection token to 0000 just like code-server to prevent conflicts.

--- dev/install/plutojl.sh
+++ dev/install/plutojl.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-julia -e 'import Pkg; Pkg.add("Pluto")'
+JULIA_DEPOT_PATH="/opt/julia/local/share/julia"
+mkdir -p $JULIA_DEPOT_PATH
+JULIA_DEPOT_PATH=$JULIA_DEPOT_PATH julia -e 'import Pkg; Pkg.add("Pluto")'
