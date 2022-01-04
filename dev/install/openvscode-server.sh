209ef4604b319e52f803d0d4aad12b75b2fce0ee
TJ Palanca <mail@tjpalanca.com>
2022-01-04 03:59:12 +0000

    Simplify

--- dev/install/openvscode-server.sh
+++ dev/install/openvscode-server.sh
@@ -3,12 +3,10 @@
 # Open VS Code Server by GitPod
 # https://github.com/gitpod-io/openvscode-server
 
-RELEASE_TAG=" openvscode-server-insiders-v1.64.0"
-RELEASE_NME="${RELEASE_TAG}-linux-x64"
-RELEASE_TAR="${RELEASE_NME}.tar.gz"
+RELEASE_TAG="openvscode-server-insiders-v1.64.0"
 RELEASE_ORG="gitpod-io"
 
-wget https://github.com/${RELEASE_ORG}/openvscode-server/releases/download/${RELEASE_TAG}/${RELEASE_TAR} && \
-    tar xzf ${RELEASE_TAG} && \
-    mv -f ${RELEASE_NME} /usr/local/share/openvscode-server && \
-    rm -f ${RELEASE_TAR}
+wget https://github.com/${RELEASE_ORG}/openvscode-server/releases/download/${RELEASE_TAG}/${RELEASE_TAG}-linux-x64.tar.gz && \
+    tar xzf ${RELEASE_TAG}-linux-x64.tar.gz && \
+    mv -f ${RELEASE_TAG}-linux-x64 /usr/local/share/openvscode-server && \
+    rm -f ${RELEASE_TAG}-linux-x64.tar.gz
