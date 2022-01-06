780aca3db585395d516d02e2861d1a987a929b2e
TJ Palanca <mail@tjpalanca.com>
2022-01-06 07:13:15 +0000

    Install

--- dev/install/code-server.sh
+++ dev/install/code-server.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/bash
 
-VERSION="4.0.1"
-
-wget "https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb" \
-	-O code-server.deb
-# wget "https://tjpalanca.sgp1.digitaloceanspaces.com/bin/code-server_${VERSION}_amd64.deb" \
-#     -O code-server.deb
-dpkg -i code-server.deb
-rm code-server.deb
+curl -fsSL https://code-server.dev/install.sh | sh -s -- version 4.0.1
