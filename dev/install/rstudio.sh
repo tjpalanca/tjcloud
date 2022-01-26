3acdf8da4e12650cd7fab35890280a1882f2f146
TJ Palanca <tjpalanca@users.noreply.github.com>
2022-01-26 20:29:35 +0800

    Update rstudio.sh

--- dev/install/rstudio.sh
+++ dev/install/rstudio.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 apt-get remove -y rstudio-server 
-wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-2021.09.1-372-amd64.deb \
+wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-2022.02.0-preview-392-amd64.deb \
     -O rstudio-server.deb 
 gdebi --non-interactive rstudio-server.deb 
 rm rstudio-server.deb
