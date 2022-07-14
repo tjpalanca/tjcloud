cf3941c2fd25cf98a5f0077a56797d576a3e2539
TJ Palanca <mail@tjpalanca.com>
2022-07-14 07:05:52 +0000

    Add missing node symlink

--- dev/install/code-server.sh
+++ dev/install/code-server.sh
@@ -2,3 +2,6 @@
 
 # Install code-server 4.5.0
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=4.5.0
+
+# Add a missing node symlink
+ln -s /usr/bin/node /usr/lib/code-server/lib/vscode/node
