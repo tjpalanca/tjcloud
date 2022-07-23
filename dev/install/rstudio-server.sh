ef945b94b8ad0f177b001c756e97ccd5faf4eb58
TJ Palanca <mail@tjpalanca.com>
2022-07-23 05:56:07 +0000

    * RStudio Preview * Remove openvscode server

--- dev/install/rstudio-server.sh
+++ dev/install/rstudio-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 apt-get remove -y rstudio-server 
-wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-2022.02.0-preview-392-amd64.deb \
+wget https://s3.amazonaws.com/rstudio-ide-build/server/bionic/amd64/rstudio-server-2022.07.0-548-amd64.deb \
     -O rstudio-server.deb 
 gdebi --non-interactive rstudio-server.deb 
 rm rstudio-server.deb
