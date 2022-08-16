2b0e782651838e416b90d005d1b97c6780d62ebc
TJ Palanca <mail@tjpalanca.com>
2022-08-16 02:28:24 +0000

    Fix unquoted string

--- modules/ingress/main.tf
+++ modules/ingress/main.tf
@@ -17,8 +17,8 @@ resource "kubernetes_namespace_v1" "ingress_nginx" {
   metadata {
     name = "ingress-nginx"
     labels = {
-      "app.kubernetes.io/instance" = ingress-nginx
-      "app.kubernetes.io/name"     = ingress-nginx
+      "app.kubernetes.io/instance" = "ingress-nginx"
+      "app.kubernetes.io/name"     = "ingress-nginx"
     }
   }
 }
