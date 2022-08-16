82c64a760ef6f2da3bec9498ecd08a1700a632ad
TJ Palanca <mail@tjpalanca.com>
2022-08-16 06:40:09 +0000

    Fetch external ips

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -13,14 +13,18 @@ data "digitalocean_kubernetes_versions" "example" {
   version_prefix = "1.23."
 }
 
-resource "digitalocean_kubernetes_cluster" "tjcloud" {
+resource "digitalocean_kubernetes_cluster" "cluster" {
   name    = var.cluster_name
   region  = "sgp1"
   version = data.digitalocean_kubernetes_versions.example.latest_version
-
   node_pool {
     name       = "worker-pool"
     size       = "s-4vcpu-8gb"
     node_count = 1
   }
+}
+
+data "digitalocean_droplet" "cluster_nodes" {
+  for_each = digitalocean_kubernetes_cluster.cluster.node_pool.0.nodes
+  name     = each.value.name
 }
--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -1,7 +1,3 @@
 output "cluster" {
-  value = {
-    urn         = digitalocean_kubernetes_cluster.tjcloud.urn
-    endpoint    = digitalocean_kubernetes_cluster.tjcloud.endpoint
-    kube_config = digitalocean_kubernetes_cluster.tjcloud.kube_config
-  }
+  value = cluster
 }
