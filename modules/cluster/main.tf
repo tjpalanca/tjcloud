cf59b66da913d9a743b6f09b7d9206e7814eea00
TJ Palanca <mail@tjpalanca.com>
2022-08-20 22:33:08 +0800

    Make the cluster smaller for n ow

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -23,7 +23,7 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   version = data.digitalocean_kubernetes_versions.versions.latest_version
   node_pool {
     name       = "worker-pool"
-    size       = "s-4vcpu-8gb"
+    size       = "s-2vcpu-4gb"
     node_count = 1
   }
 }
