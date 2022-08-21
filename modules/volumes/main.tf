c6f2694a264788afa4759fa4137b80fd93b7aaca
TJ Palanca <mail@tjpalanca.com>
2022-08-21 23:26:12 +0800

    Switch to just a claim

--- modules/volumes/main.tf
+++ modules/volumes/main.tf
@@ -7,34 +7,14 @@ terraform {
   }
 }
 
-resource "kubernetes_persistent_volume_v1" "apps" {
+resource "kubernetes_persistent_volume_claim_v1" "apps" {
   metadata {
     name = "apps"
   }
   spec {
-    capacity = {
-      storage = "1Gi"
-    }
-    volume_mode        = "Filesystem"
     access_modes       = ["ReadWriteOnce"]
     storage_class_name = "do-block-storage"
-    persistent_volume_source {
-      csi {
-        driver        = "dobs.csi.digitalocean.com"
-        volume_handle = "apps"
-        fs_type       = "ext4"
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim_v1" "apps_claim" {
-  metadata {
-    name = "apps-claim"
-  }
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "do-block-storage"
+    volume_name        = "pvc-13e1990e-08eb-457e-bdfe-1173423fa768"
     resources {
       requests = {
         storage = "50Mi"
