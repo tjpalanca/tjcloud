10252b6e5e971147734e593e2a9850564dcc9e8e
TJ Palanca <mail@tjpalanca.com>
2022-08-22 00:25:37 +0800

    A

--- modules/pgadmin/main.tf
+++ modules/pgadmin/main.tf
@@ -89,6 +89,6 @@ resource "kubernetes_persistent_volume_claim_v1" "apps" {
         storage = "1Gi"
       }
     }
-    volume_name = kubernetes_persistent_volume_v1.apps.metadata.0.name
+    volume_name = "apps"
   }
 }
