e545fbbdeb179486dd60cca73345a6c34ddcdb73
TJ Palanca <mail@tjpalanca.com>
2022-08-23 01:26:30 +0800

    Try

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -22,15 +22,19 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   region  = var.do_region
   version = data.digitalocean_kubernetes_versions.versions.latest_version
   node_pool {
-    name       = "stateful"
-    size       = "s-2vcpu-4gb"
+    name       = "default"
+    size       = "s-4vcpu-8gb"
     node_count = 1
   }
 }
 
+data "digitalocean_kubernetes_cluster" "cluster" {
+  name = var.cluster_name
+}
+
 data "digitalocean_droplet" "nodes" {
   for_each = toset([
-    for node in digitalocean_kubernetes_cluster.cluster.node_pool.0.nodes :
+    for node in data.digitalocean_kubernetes_cluster.cluster.node_pool.0.nodes :
     node.name
   ])
   name = each.value
@@ -47,11 +51,14 @@ resource "cloudflare_record" "nodes" {
   value    = each.value.ipv4_address
   type     = "A"
   proxied  = true
+  depends_on = [
+    digitalocean_kubernetes_cluster.cluster
+  ]
 }
 
 resource "digitalocean_firewall" "enable_web_access" {
-  name = "k8s-web-access-${digitalocean_kubernetes_cluster.cluster.id}"
-  tags = ["k8s:${digitalocean_kubernetes_cluster.cluster.id}"]
+  name = "k8s-web-access-${data.digitalocean_kubernetes_cluster.cluster.id}"
+  tags = ["k8s:${data.digitalocean_kubernetes_cluster.cluster.id}"]
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
@@ -62,4 +69,7 @@ resource "digitalocean_firewall" "enable_web_access" {
     port_range       = "443"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
+  depends_on = [
+    digitalocean_kubernetes_cluster.cluster
+  ]
 }
--- modules/database/postgres.tf
+++ modules/database/postgres.tf
@@ -1,14 +1,14 @@
-module "main_postgres_database" {
-  source = "../../resources/postgres"
-  database = {
-    username   = var.main_postgres_username
-    password   = var.main_postgres_password
-    db_name    = var.main_postgres_database
-    db_version = "14"
-    storage    = "5Gi"
-  }
-  config = {
-    name      = "main"
-    namespace = kubernetes_namespace_v1.database.metadata.0.name
-  }
-}
+# module "main_postgres_database" {
+#   source = "../../resources/postgres"
+#   database = {
+#     username   = var.main_postgres_username
+#     password   = var.main_postgres_password
+#     db_name    = var.main_postgres_database
+#     db_version = "14"
+#     storage    = "5Gi"
+#   }
+#   config = {
+#     name      = "main"
+#     namespace = kubernetes_namespace_v1.database.metadata.0.name
+#   }
+# }
