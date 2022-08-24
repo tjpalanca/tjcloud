2bba7eb4c924b01131ae1734178f6910ce77e168
TJ Palanca <mail@tjpalanca.com>
2022-08-24 08:36:23 +0800

    Set up cluster

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,5 +1,4 @@
 terraform {
-
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
@@ -10,11 +9,17 @@ terraform {
       version = "~> 3.0"
     }
   }
-
 }
 
 data "digitalocean_kubernetes_versions" "versions" {
   version_prefix = "1.23."
+}
+
+locals {
+  node_count = {
+    production  = 1
+    development = 1
+  }
 }
 
 resource "digitalocean_kubernetes_cluster" "cluster" {
@@ -22,42 +27,32 @@ resource "digitalocean_kubernetes_cluster" "cluster" {
   region  = var.do_region
   version = data.digitalocean_kubernetes_versions.versions.latest_version
   node_pool {
-    name       = "stateless"
-    size       = "s-1vcpu-2gb"
+    name       = "production"
+    size       = "s-2vcpu-4gb"
     node_count = 1
     taint {
-      key    = "workload"
-      value  = "stateful"
+      key    = "environment"
+      value  = "development"
       effect = "NoExecute"
     }
   }
 }
 
-resource "digitalocean_kubernetes_node_pool" "worker" {
+resource "digitalocean_kubernetes_node_pool" "development" {
   cluster_id = digitalocean_kubernetes_cluster.cluster.id
-  name       = "stateful"
+  name       = "development"
   size       = "s-2vcpu-4gb"
-  node_count = 1
+  node_count = local.node_count.development
   taint {
-    key    = "workload"
-    value  = "stateless"
+    key    = "environment"
+    value  = "production"
     effect = "NoExecute"
   }
 }
 
-data "digitalocean_kubernetes_cluster" "cluster" {
-  name = var.cluster_name
-  depends_on = [
-    digitalocean_kubernetes_cluster.cluster
-  ]
-}
-
-data "digitalocean_droplet" "nodes" {
-  for_each = toset([
-    for node in data.digitalocean_kubernetes_cluster.cluster.node_pool.0.nodes :
-    node.name
-  ])
-  name = each.value
+data "digitalocean_droplet" "production_nodes" {
+  count = local.node_count.production
+  name  = digitalocean_kubernetes_cluster.cluster.node_pool.0.nodes[count.index].name
 }
 
 data "cloudflare_zone" "main" {
@@ -65,20 +60,20 @@ data "cloudflare_zone" "main" {
 }
 
 resource "cloudflare_record" "nodes" {
-  for_each = data.digitalocean_droplet.nodes
-  zone_id  = data.cloudflare_zone.main.zone_id
-  name     = "@"
-  value    = each.value.ipv4_address
-  type     = "A"
-  proxied  = true
+  count   = local.node_count.production
+  zone_id = data.cloudflare_zone.main.zone_id
+  name    = "@"
+  value   = data.digitalocean_droplet.production_nodes[count.index].ipv4_address
+  type    = "A"
+  proxied = true
   depends_on = [
     digitalocean_kubernetes_cluster.cluster
   ]
 }
 
 resource "digitalocean_firewall" "enable_web_access" {
-  name = "k8s-web-access-${data.digitalocean_kubernetes_cluster.cluster.id}"
-  tags = ["k8s:${data.digitalocean_kubernetes_cluster.cluster.id}"]
+  name = "k8s-web-access-${digitalocean_kubernetes_cluster.cluster.id}"
+  tags = ["k8s:${digitalocean_kubernetes_cluster.cluster.id}"]
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
@@ -89,7 +84,4 @@ resource "digitalocean_firewall" "enable_web_access" {
     port_range       = "443"
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
-  depends_on = [
-    digitalocean_kubernetes_cluster.cluster
-  ]
 }
