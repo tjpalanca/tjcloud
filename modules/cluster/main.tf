4121c4e827d70b8723bea8910f9516e30b2f2b4e
TJ Palanca <mail@tjpalanca.com>
2022-08-27 14:19:41 +0800

    Add tag to cluster

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -4,6 +4,21 @@ terraform {
       source  = "linode/linode"
       version = "1.29.2"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+locals {
+  production = {
+    node_count  = 1
+    volume_size = 10
+  }
+  development = {
+    node_count  = 1
+    volume_size = 40
   }
 }
 
@@ -11,9 +26,11 @@ resource "linode_lke_cluster" "cluster" {
   label       = var.cluster_name
   k8s_version = "1.23"
   region      = var.linode_region
-
   pool {
     type  = "g6-standard-4"
     count = 1
+    tags = {
+      cluster = var.cluster_name
+    }
   }
 }
