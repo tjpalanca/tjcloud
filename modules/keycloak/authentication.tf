515c8c584321ce93ff2d29061e6080ca977e43b8
TJ Palanca <mail@tjpalanca.com>
2022-09-02 00:20:23 +0800

    A

--- modules/keycloak/authentication.tf
+++ modules/keycloak/authentication.tf
@@ -10,9 +10,9 @@ resource "keycloak_authentication_execution" "review_profile" {
   requirement       = "REQUIRED"
 }
 
-resource "keycloak_authentication_subflow" "link_or_create_user" {
+resource "keycloak_authentication_subflow" "create_or_link_user" {
   realm_id          = keycloak_realm.main.id
-  alias             = "djdjdjjddjdj"
+  alias             = "Create or link user"
   parent_flow_alias = keycloak_authentication_flow.autolink.alias
   provider_id       = "basic-flow"
   requirement       = "REQUIRED"
@@ -23,18 +23,18 @@ resource "keycloak_authentication_subflow" "link_or_create_user" {
 
 resource "keycloak_authentication_execution" "create_user_if_unique" {
   realm_id          = keycloak_realm.main.id
-  parent_flow_alias = keycloak_authentication_subflow.link_or_create_user.alias
+  parent_flow_alias = keycloak_authentication_subflow.create_or_link_user.alias
   authenticator     = "idp-create-user-if-unique"
   requirement       = "ALTERNATIVE"
   depends_on = [
-    keycloak_authentication_subflow.link_or_create_user
+    keycloak_authentication_subflow.create_or_link_user
   ]
 }
 
-resource "keycloak_authentication_subflow" "handle_existing_account" {
+resource "keycloak_authentication_subflow" "handle_an_existing_account" {
   realm_id          = keycloak_realm.main.id
-  alias             = "Handle existing account"
-  parent_flow_alias = keycloak_authentication_subflow.link_or_create_user.alias
+  alias             = "Handle an existing account"
+  parent_flow_alias = keycloak_authentication_subflow.create_or_link_user.alias
   provider_id       = "basic-flow"
   requirement       = "ALTERNATIVE"
   depends_on = [
@@ -44,17 +44,17 @@ resource "keycloak_authentication_subflow" "handle_existing_account" {
 
 resource "keycloak_authentication_execution" "confirm_link_existing_account" {
   realm_id          = keycloak_realm.main.id
-  parent_flow_alias = keycloak_authentication_subflow.handle_existing_account.alias
+  parent_flow_alias = keycloak_authentication_subflow.handle_an_existing_account.alias
   authenticator     = "idp-confirm-link"
   requirement       = "REQUIRED"
   depends_on = [
-    keycloak_authentication_subflow.handle_existing_account
+    keycloak_authentication_subflow.handle_an_existing_account
   ]
 }
 
 resource "keycloak_authentication_execution" "automatically_set_existing_user" {
   realm_id          = keycloak_realm.main.id
-  parent_flow_alias = keycloak_authentication_subflow.handle_existing_account.alias
+  parent_flow_alias = keycloak_authentication_subflow.handle_an_existing_account.alias
   authenticator     = "idp-auto-link"
   requirement       = "REQUIRED"
   depends_on = [
