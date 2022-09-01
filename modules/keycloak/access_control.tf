1a675a87589705d07b0f73447a09015ed647f656
TJ Palanca <mail@tjpalanca.com>
2022-09-02 00:38:18 +0800

    Hello

--- modules/keycloak/access_control.tf
+++ modules/keycloak/access_control.tf
@@ -12,3 +12,17 @@ resource "keycloak_group" "tester" {
   realm_id = keycloak_realm.main.id
   name     = "Tester"
 }
+
+resource "keycloak_user" "admin_users" {
+  for_each = var.admin_emails
+  realm_id = keycloak_realm.main.id
+  username = each.value
+  enabled  = true
+  email    = each.value
+}
+
+resource "keycloak_group_memberships" "admin_users" {
+  realm_id = keycloak_realm.realm.id
+  group_id = keycloak_group.administrator.id
+  members  = var.admin_emails
+}
