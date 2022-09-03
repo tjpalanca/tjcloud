c526d72ddbf6775db29a3a2ec0b21267d034b3be
TJ Palanca <mail@tjpalanca.com>
2022-09-03 16:15:29 +0800

    Login theme

--- modules/keycloak/realms.tf
+++ modules/keycloak/realms.tf
@@ -1,6 +1,7 @@
 resource "keycloak_realm" "main" {
   realm        = var.settings.realm_name
   display_name = var.settings.display_name
+  login_theme  = "social"
   depends_on = [
     module.keycloak
   ]
