11edb0fc27cfea451c4d484e693172eca0c8549b
TJ Palanca <mail@tjpalanca.com>
2022-09-05 22:35:24 +0800

    IncludedCustomAudience

--- elements/gateway/main.tf
+++ elements/gateway/main.tf
@@ -46,11 +46,12 @@ resource "keycloak_openid_client_default_scopes" "default_client_scopes" {
 }
 
 resource "keycloak_openid_audience_protocol_mapper" "audience_mapper" {
-  realm_id            = data.keycloak_realm.realm.id
-  client_id           = keycloak_openid_client.client.id
-  name                = "audience"
-  add_to_access_token = true
-  add_to_id_token     = false
+  realm_id                 = data.keycloak_realm.realm.id
+  client_id                = keycloak_openid_client.client.id
+  included_custom_audience = keycloak_openid_client.client.id
+  name                     = "audience"
+  add_to_access_token      = true
+  add_to_id_token          = false
 }
 
 resource "random_password" "cookie_secret" {
