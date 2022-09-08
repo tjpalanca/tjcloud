8fcb9b28ae00a34acbf08e64ee630fa7ad0218cd
TJ Palanca <mail@tjpalanca.com>
2022-09-08 11:51:22 +0800

    Add code port gateway

--- modules/code/main.tf
+++ modules/code/main.tf
@@ -109,20 +109,20 @@ module "code_gateway" {
   default_client_scopes = var.default_client_scopes
 }
 
-# module "code_port_gateway" {
-#   for_each = toset(["3838", "5500", "8888"])
-#   source   = "../../elements/gateway"
-#   host     = each.value
-#   zone     = var.cloudflare_zone
-#   service = merge(
-#     module.code_application.service,
-#     { port = tonumber(each.value) }
-#   )
-#   keycloak_realm_name   = var.keycloak_realm_name
-#   keycloak_url          = var.keycloak_url
-#   keycloak_groups       = ["Administrator"]
-#   default_client_scopes = var.default_client_scopes
-# }
+module "code_port_gateway" {
+  for_each = toset(["3838", "5500", "8888"])
+  source   = "../../elements/gateway"
+  host     = each.value
+  zone     = var.cloudflare_zone
+  service = merge(
+    module.code_application.service,
+    { port = tonumber(each.value) }
+  )
+  keycloak_realm_name   = var.keycloak_realm_name
+  keycloak_url          = var.keycloak_url
+  keycloak_groups       = ["Administrator"]
+  default_client_scopes = var.default_client_scopes
+}
 
 # module "code_test_gateway" {
 #   count  = 1
