878b26c1ae54e2052a5ffa9db2166e6288074690
TJ Palanca <mail@tjpalanca.com>
2022-09-08 10:29:16 +0800

    A

--- elements/application/main.tf
+++ elements/application/main.tf
@@ -55,7 +55,7 @@ resource "kubernetes_deployment_v1" "deployment" {
           dynamic "port" {
             for_each = var.ports
             content {
-              name           = "${var.name}-${port.value}"
+              name           = port.value
               container_port = port.value
             }
           }
