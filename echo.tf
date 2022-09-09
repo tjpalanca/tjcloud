7a7c6c1842770cefb8aedb86d9f307b3ec646449
TJ Palanca <mail@tjpalanca.com>
2022-09-09 11:17:20 +0800

    PGAdmin and ECHO

--- echo.tf
+++ echo.tf
@@ -1,11 +1,11 @@
-# module "echo" {
-#   source                = "./modules/echo"
-#   cloudflare_zone       = var.main_cloudflare_zone
-#   keycloak_realm_name   = module.keycloak_config.realms.main.realm
-#   keycloak_url          = module.keycloak.url
-#   default_client_scopes = [module.keycloak_config.client_scopes.groups.name]
-#   depends_on = [
-#     module.cluster,
-#     module.keycloak
-#   ]
-# }
+module "echo" {
+  source                = "./modules/echo"
+  cloudflare_zone       = var.main_cloudflare_zone
+  keycloak_realm_name   = module.keycloak_config.realms.main.realm
+  keycloak_url          = module.keycloak.url
+  default_client_scopes = [module.keycloak_config.client_scopes.groups.name]
+  depends_on = [
+    module.cluster,
+    module.keycloak
+  ]
+}
--- pgadmin.tf
+++ pgadmin.tf
@@ -1,17 +1,17 @@
-# module "pgadmin" {
-#   source                   = "./modules/pgadmin"
-#   pgadmin_default_username = var.pgadmin_default_username
-#   pgadmin_default_password = var.pgadmin_default_password
-#   pgadmin_cloudflare_zone  = var.main_cloudflare_zone
-#   keycloak_realm_name      = module.keycloak_config.realms.main.realm
-#   keycloak_url             = module.keycloak.url
-#   default_client_scopes    = [module.keycloak_config.client_scopes.groups.name]
-#   volume_name              = module.cluster.main_node_volume.label
-#   node_ip_address          = module.cluster.main_node.ip_address
-#   node_password            = var.root_password
-#   node_name                = module.cluster.main_node.label
-#   depends_on = [
-#     module.cluster,
-#     module.keycloak
-#   ]
-# }
+module "pgadmin" {
+  source                   = "./modules/pgadmin"
+  pgadmin_default_username = var.pgadmin_default_username
+  pgadmin_default_password = var.pgadmin_default_password
+  pgadmin_cloudflare_zone  = var.main_cloudflare_zone
+  keycloak_realm_name      = module.keycloak_config.realms.main.realm
+  keycloak_url             = module.keycloak.url
+  default_client_scopes    = [module.keycloak_config.client_scopes.groups.name]
+  volume_name              = module.cluster.main_node_volume.label
+  node_ip_address          = module.cluster.main_node.ip_address
+  node_password            = var.root_password
+  node_name                = module.cluster.main_node.label
+  depends_on = [
+    module.cluster,
+    module.keycloak
+  ]
+}
