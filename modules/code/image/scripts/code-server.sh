8f623bbdb5346b90ccb8e0625514fa1a3748c9f4
TJ Palanca <mail@tjpalanca.com>
2022-09-18 22:17:17 +0800

    Upgrade code-server to 4.7.0

--- modules/code/image/scripts/code-server.sh
+++ modules/code/image/scripts/code-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Install code-server 4.5.1
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=4.6.1
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --version=4.7.0
 
 # Add a missing node symlink
 ln -s /usr/bin/node /usr/lib/code-server/lib/vscode/node
