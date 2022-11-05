1a76e3beecc7f2bb87ef30973020b1473fc4b78c
TJ P <mail@tjpalanca.com>
2022-11-05 16:49:43 +0000

    Add command

--- modules/mastodon/main.tf
+++ modules/mastodon/main.tf
@@ -32,6 +32,10 @@ module "mastodon_application" {
   service_type = "ClusterIP"
   ports        = [3000]
   image        = "ghcr.io/mastodon/mastodon:latest"
+  command = [
+    "bundle exec rails db:migrate;",
+    "bundle exec rails s -p 3000"
+  ]
   env_vars = {
     LOCAL_DOMAIN = var.cloudflare_zone_name
     # WEB_DOMAIN   = "${local.host}.${var.cloudflare_zone_name}"
@@ -41,20 +45,20 @@ module "mastodon_application" {
     VAPID_PUBLIC_KEY         = var.vapid_public_key
     RAILS_ENV                = "production"
     RAILS_SERVE_STATIC_FILES = "true"
-    TRUSTED_PROXY_IP         = ""
-    SINGLE_USER_MODE         = "true"
-    DEFAULT_LOCALE           = "en"
-    SMTP_SERVER              = var.smtp_server
-    SMTP_PORT                = tostring(var.smtp_port)
-    SMTP_FROM_ADDRESS        = "mastodon@${var.cloudflare_zone_name}"
-    DB_HOST                  = var.postgres_host
-    DB_USER                  = var.postgres_user
-    DB_PASS                  = var.postgres_pass
-    DB_NAME                  = postgresql_database.mastodon.name
-    DB_PORT                  = tostring(var.postgres_port)
-    REDIS_HOST               = var.redis_host
-    REDIS_PORT               = tostring(var.redis_port)
-    ES_ENABLED               = "false"
+    # TRUSTED_PROXY_IP         = ""
+    SINGLE_USER_MODE  = "true"
+    DEFAULT_LOCALE    = "en"
+    SMTP_SERVER       = var.smtp_server
+    SMTP_PORT         = tostring(var.smtp_port)
+    SMTP_FROM_ADDRESS = "mastodon@${var.cloudflare_zone_name}"
+    DB_HOST           = var.postgres_host
+    DB_USER           = var.postgres_user
+    DB_PASS           = var.postgres_pass
+    DB_NAME           = postgresql_database.mastodon.name
+    DB_PORT           = tostring(var.postgres_port)
+    REDIS_HOST        = var.redis_host
+    REDIS_PORT        = tostring(var.redis_port)
+    ES_ENABLED        = "false"
   }
 }
 
