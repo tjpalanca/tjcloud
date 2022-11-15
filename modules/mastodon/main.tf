e581275cc914a69f9df81afa8a164cfe1812a384
TJ Palanca <tjpalanca@users.noreply.github.com>
2022-11-15 14:20:25 +0800

    Mastodon Upgrades (#25)
    * #24 Upgrade Mastodon to v4.0.1
    
    * Perform upgrade
    
    * Liveness and readiness probes for Mastodon #20
    
    * Add readiness and liveness probes to the streaming and web images
    
    * Change Puma to run in Single Mode
    
    WEB_CONCURRENCY=0

--- modules/mastodon/main.tf
+++ modules/mastodon/main.tf
@@ -21,35 +21,36 @@ locals {
   host  = "mastodon"
   image = "tootsuite/mastodon:${var.mastodon_version}"
   envs = {
-    LOCAL_DOMAIN             = var.cloudflare_zone_name
-    WEB_DOMAIN               = "${local.host}.${var.cloudflare_zone_name}"
-    SECRET_KEY_BASE          = var.secret_key_base
-    OTP_SECRET               = var.otp_secret
-    VAPID_PRIVATE_KEY        = var.vapid_private_key
-    VAPID_PUBLIC_KEY         = var.vapid_public_key
-    RAILS_ENV                = "production"
-    NODE_ENV                 = "production"
-    RAILS_SERVE_STATIC_FILES = "true"
-    STREAMING_API_BASE_URL   = "wss://${local.host}-streaming.${var.cloudflare_zone_name}"
-    SINGLE_USER_MODE         = "true"
-    DEFAULT_LOCALE           = "en"
-    SMTP_SERVER              = var.smtp_server
-    SMTP_PORT                = tostring(var.smtp_port)
-    SMTP_FROM_ADDRESS        = "mastodon@${var.cloudflare_zone_name}"
-    DB_HOST                  = var.postgres_host
-    DB_USER                  = var.postgres_user
-    DB_PASS                  = var.postgres_pass
-    DB_NAME                  = postgresql_database.mastodon.name
-    DB_PORT                  = tostring(var.postgres_port)
-    REDIS_HOST               = var.redis_host
-    REDIS_PORT               = tostring(var.redis_port)
-    REDIS_NAMESPACE          = "mastodon"
-    ES_ENABLED               = "false"
-    TRUSTED_PROXY_IP         = "10.2.0.9"
-    RAILS_LOG_LEVEL          = "warn"
-    WEB_CONCURRENCY          = 1
-    MAX_THREADS              = 4
-    STREAMING_CLUSTER_NUM    = 2
+    LOCAL_DOMAIN                    = var.cloudflare_zone_name
+    WEB_DOMAIN                      = "${local.host}.${var.cloudflare_zone_name}"
+    SECRET_KEY_BASE                 = var.secret_key_base
+    OTP_SECRET                      = var.otp_secret
+    VAPID_PRIVATE_KEY               = var.vapid_private_key
+    VAPID_PUBLIC_KEY                = var.vapid_public_key
+    RAILS_ENV                       = "production"
+    NODE_ENV                        = "production"
+    RAILS_SERVE_STATIC_FILES        = "true"
+    STREAMING_API_BASE_URL          = "wss://${local.host}-streaming.${var.cloudflare_zone_name}"
+    SINGLE_USER_MODE                = "true"
+    DEFAULT_LOCALE                  = "en"
+    SMTP_SERVER                     = var.smtp_server
+    SMTP_PORT                       = tostring(var.smtp_port)
+    SMTP_FROM_ADDRESS               = "mastodon@${var.cloudflare_zone_name}"
+    DB_HOST                         = var.postgres_host
+    DB_USER                         = var.postgres_user
+    DB_PASS                         = var.postgres_pass
+    DB_NAME                         = postgresql_database.mastodon.name
+    DB_PORT                         = tostring(var.postgres_port)
+    REDIS_HOST                      = var.redis_host
+    REDIS_PORT                      = tostring(var.redis_port)
+    REDIS_NAMESPACE                 = "mastodon"
+    ES_ENABLED                      = "false"
+    TRUSTED_PROXY_IP                = "10.2.0.9"
+    RAILS_LOG_LEVEL                 = "warn"
+    WEB_CONCURRENCY                 = 0
+    MAX_THREADS                     = 4
+    STREAMING_CLUSTER_NUM           = 2
+    SKIP_POST_DEPLOYMENT_MIGRATIONS = var.skip_post_deployment ? "true" : "false"
   }
   host_path = "/mnt/${var.volume_name}/mastodon/"
   vols = [{
@@ -57,6 +58,19 @@ locals {
     mount_path  = "/mastodon/public/system"
     host_path   = local.host_path
     mount_type  = "DirectoryOrCreate"
+  }]
+  web_port = 3000
+  web_probes = [{
+    path                  = "/health"
+    port                  = local.web_port
+    initial_delay_seconds = 10
+    period_seconds        = 10
+    timeout_seconds       = 10
+  }]
+  streaming_port = 4000
+  streaming_probes = [{
+    path = "/api/v1/streaming/health"
+    port = local.streaming_port
   }]
 }
 
@@ -73,17 +87,19 @@ module "mastodon_permissions" {
 }
 
 module "mastodon_application" {
-  source    = "../../elements/application"
-  name      = "mastodon"
-  namespace = kubernetes_namespace_v1.mastodon.metadata[0].name
-  ports     = [3000]
-  image     = local.image
-  env_vars  = local.envs
-  node_name = var.node_name
-  volumes   = local.vols
+  source           = "../../elements/application"
+  name             = "mastodon"
+  namespace        = kubernetes_namespace_v1.mastodon.metadata[0].name
+  ports            = [local.web_port]
+  image            = local.image
+  env_vars         = local.envs
+  node_name        = var.node_name
+  volumes          = local.vols
+  liveness_probes  = local.web_probes
+  readiness_probes = local.web_probes
   command = [
     "bash", "-c",
-    "bundle exec rails db:migrate; bundle exec rails s -p 3000;"
+    "bundle exec rails db:migrate; bundle exec rails s -p ${local.web_port};"
   ]
   depends_on = [
     module.mastodon_permissions
@@ -100,13 +116,15 @@ module "mastodon_ingress" {
 }
 
 module "mastodon_streaming" {
-  source    = "../../elements/application"
-  name      = "mastodon-streaming"
-  namespace = kubernetes_namespace_v1.mastodon.metadata[0].name
-  ports     = [4000]
-  image     = local.image
-  env_vars  = local.envs
-  command   = ["node", "./streaming"]
+  source           = "../../elements/application"
+  name             = "mastodon-streaming"
+  namespace        = kubernetes_namespace_v1.mastodon.metadata[0].name
+  ports            = [local.streaming_port]
+  image            = local.image
+  env_vars         = local.envs
+  command          = ["node", "./streaming"]
+  liveness_probes  = local.streaming_probes
+  readiness_probes = local.streaming_probes
 }
 
 module "mastodon_streaming_ingress" {
