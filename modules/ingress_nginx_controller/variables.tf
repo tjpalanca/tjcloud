618305bbe52df74e3f6b6f39e1ebf0720f5c666c
TJ Palanca <mail@tjpalanca.com>
2022-11-25 11:00:40 +0800

    Upgrade NGINX Ingress Controller Fixes #27
    Upgrade to v1.3.1

--- modules/ingress_nginx_controller/variables.tf
+++ modules/ingress_nginx_controller/variables.tf
@@ -14,3 +14,13 @@ variable "server_snippets" {
   type    = list(string)
   default = []
 }
+
+variable "controller_version" {
+  type    = string
+  default = "1.4.0"
+}
+
+variable "certgen_version" {
+  type    = string
+  default = "20220916-gd32f8c343"
+}
