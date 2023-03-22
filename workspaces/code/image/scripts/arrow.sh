7962c268e51da1a87b7c864982c6c1cf89c9b5ea
TJ Palanca <mail@tjpalanca.com>
2023-03-22 17:53:06 +0800

    New arrow script

--- workspaces/code/image/scripts/arrow.sh
+++ workspaces/code/image/scripts/arrow.sh
@@ -4,10 +4,8 @@ apt update
 apt install -y -V ca-certificates lsb-release wget
 wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
 apt install -y -V ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
-rm ./apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
 apt update
 apt install -y -V libarrow-dev # For C++
-apt install -y -V libarrow-python-dev # For Python
 apt install -y -V libarrow-glib-dev # For GLib (C)
 apt install -y -V libarrow-dataset-dev # For Apache Arrow Dataset C++
 apt install -y -V libarrow-dataset-glib-dev # For Apache Arrow Dataset GLib (C)
