21b467a976d72d4a0639a92e612bfeb891d976c1
TJ Palanca <mail@tjpalanca.com>
2023-03-22 08:44:53 +0800

    Docker

---
+++ workspaces/keycloak/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "~> 1.17.1"
+    }
+  }
+}
+
+resource "postgresql_database" "keycloak" {
+  name = "keycloak"
+}
+
+resource "kubernetes_namespace_v1" "keycloak" {
+  metadata {
+    name = "keycloak"
+  }
+}
+
+module "keycloak_application" {
+  source       = "../../elements/application"
+  name         = "keycloak"
+  namespace    = kubernetes_namespace_v1.keycloak.metadata.0.name
+  service_type = "ClusterIP"
+  ports        = [8080]
+  replicas     = 1
+  command_args = ["start-dev"]
+  image        = var.keycloak.image
+  env_vars = {
+    KC_DB                   = "postgres"
+    KC_DB_URL_HOST          = var.database.internal_name
+    KC_DB_URL_PORT          = var.database.internal_port
+    KC_DB_URL_DATABASE      = postgresql_database.keycloak.name
+    KC_DB_USERNAME          = var.database.username
+    KC_DB_PASSWORD          = var.database.password
+    KEYCLOAK_ADMIN          = var.admin.username
+    KEYCLOAK_ADMIN_PASSWORD = var.admin.password
+    KC_PROXY                = "edge"
+  }
+  readiness_probes = [{
+    path = "/realms/master"
+    port = 8080
+  }]
+}
+
+module "keycloak_ingress" {
+  source    = "../../elements/ingress"
+  host      = var.keycloak.subdomain
+  zone_id   = var.keycloak.cloudflare_zone_id
+  zone_name = var.keycloak.cloudflare_zone_name
+  service   = module.keycloak_application.service
+  annotations = {
+    "nginx.ingress.kubernetes.io/proxy-buffer-size" = "256k"
+    "nginx.ingress.kubernetes.io/enable-cors"       = "true"
+  }
+}
