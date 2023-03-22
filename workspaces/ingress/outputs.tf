bdeb224cb3467772d0e70d45c10e2fdb219aa763
TJ Palanca <mail@tjpalanca.com>
2023-03-22 17:39:00 +0800

    Remove ingress

--- workspaces/ingress/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "authenticated_origin_pulls_secret" {
-  value = "${kubernetes_secret_v1.authenticated_origin_pull_ca.metadata[0].namespace}/${kubernetes_secret_v1.authenticated_origin_pull_ca.metadata[0].name}"
-}
