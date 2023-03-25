d619d3e234745f0597a79494ba3b62b60ef8c159
TJ P <mail@tjpalanca.com>
2023-03-25 09:06:37 +0000

    Remove R2 buckets from management

--- workspaces/cloudflare/main.tf
+++ workspaces/cloudflare/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.60.0"
-    }
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "4.1.0"
