283129cb9b1d8cefde5924e535aad9d635c9e19d
TJ P <mail@tjpalanca.com>
2023-03-27 02:56:58 +0000

    Upgrade quarto

--- workspaces/code/image/scripts/quarto.sh
+++ workspaces/code/image/scripts/quarto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Download quarto
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.2.247/quarto-1.2.247-linux-amd64.deb \
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.290/quarto-1.3.290-linux-amd64.deb \
     -O quarto.deb
 
 # Install deb file
